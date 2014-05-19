0d3b3093334fe327a458ba1d8f29e37b0d1a777a
Caleb <caleb.brazier@gmail.com>
2014-05-19 01:20:45 -0400

    First project upload
    Signed-off-by: Caleb <caleb.brazier@gmail.com>

---
+++ src/project/caleb/antoine/HelloWorld.java
@@ -0,0 +1,13 @@
+package project.caleb.antoine;
+
+public class HelloWorld {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+      System.out.println("Hello World Mutha Fukka!");
+   }
+
+}
