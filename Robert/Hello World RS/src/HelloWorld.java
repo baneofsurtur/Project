2a32f8855bc7e961635b58d51757453b8a8d4597
rdsmart <rdsmart@umich.edu>
2014-05-20 20:34:25 -0400

    Fixed Folder Hierarchy

---
+++ Robert/Hello World RS/src/HelloWorld.java
@@ -0,0 +1,24 @@
+//Hello World Test Application
+//Robert Smart
+
+import javax.swing.*;
+
+//Main Class
+public class HelloWorld {
+	
+	////////////////////////////////////////////////
+	public static void main(String[] args) {
+		
+		//Print Hello World.
+		System.out.println("Hello World");
+		
+		//Create New Instance of popUp Class
+		popUp newWindow = new popUp();
+		
+		//Show Frame On Screen
+		newWindow.ShowFrame();
+	}//end main
+	///////////////////////////////////////////////
+	
+}//end HelloWorld
+
